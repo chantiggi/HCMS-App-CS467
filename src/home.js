c29f4f8bba8a70e837084b59087ddaf3eab99073
Jennifer Aldover <aldover27@gmail.com>
2019-08-03 04:09:11 -0400

    styled landing page

--- src/home.js
+++ src/home.js
@@ -7,7 +7,7 @@ export class Home extends React.Component {
         return (
             <div className="container">
                 <h3><span> Welcome to the Horse Care Management System.</span></h3>
-                <h5> HCMS is an information keeping system that provides support for the following:  </h5>
+                <h5 className="subheading"> HCMS is an information keeping system that provides support for the following:  </h5>
                 <ul className="list">
                     <li> Managing Horses </li>
                     <li> Manging Feeds </li>
