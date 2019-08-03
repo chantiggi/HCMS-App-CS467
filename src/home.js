58edb9f9f484b7f038742fc0a05625e64354853b
Jennifer Aldover <aldover27@gmail.com>
2019-08-02 20:27:18 -0400

    modified styles with specific classes to avoid mixing local styles with global styles

--- src/home.js
+++ src/home.js
@@ -6,7 +6,6 @@ export class Home extends React.Component {
     render() {
         return (
             <div className="container">
-                <img src={image} />
                 <h3><span> Welcome to the Horse Care Management System.</span></h3>
                 <h5> HCMS is an information keeping system that provides support for the following:  </h5>
                 <ul className="list">
--- src/org_reg.js
+++ src/org_reg.js
@@ -4,7 +4,7 @@ import './org_reg.css';
 export class OrgReg extends React.Component {
 	render() {
 		return (
-			<div className="container">
+			<div className="container orgRegForm">
 				<p>
 					<form>
 						<div className="row">
