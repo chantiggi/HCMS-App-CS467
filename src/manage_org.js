0eb5a339385f113903de67bc795df2784b7409b6
Jennifer Aldover <aldover27@gmail.com>
2019-08-03 10:22:36 -0400

    modified form and replaced dropdowns with select elements

--- src/manage_org.js
+++ src/manage_org.js
@@ -12,117 +12,106 @@ export class ManageOrg extends React.Component {
 				<div className="tab-content org" id="">
 					<form>
 						<h5>Organization Information</h5>
-						<label for="onanme"><i className="fa fa-envelope"></i>Name</label>
-						<input type="text" id="oname" name="oname" />
-
-						<label for="oaddress"><i className="fa fa-envelope"></i>Address</label>
-						<input type="text" id="oaddress" name="oaddress" />
-
-						<div className="row">
-							<div className="col-25">
-								<label for="city">City</label>
-								<input type="text" id="city" name="city" />
-							</div>
-							<div className="col-25">
-								<label for="state">State</label>
-								<input type="text" id="state" name="state" />
-							</div>
-							<div className="col-25">
-								<label for="zip">Zip Code</label>
-								<input type="text" id="zip" name="zip" />
-							</div>
+						<div className="form-group">
+							<label for="oname"><i className="fa fa-envelope"></i>Name</label>
+							<input type="text" id="oname" name="oname" />
 						</div>
-						<input type="submit" value="Submit Changes" className="btns addbtn" />
+
+						<div className="form-group">
+							<label for="oaddress"><i className="fa fa-envelope"></i>Address</label>
+							<input type="text" id="oaddress" name="oaddress" />
+						</div>
+
+						<div className="form-group">
+							<label for="city">City</label>
+							<input type="text" id="city" name="city" />
+						</div>
+
+						<div className="form-group">
+							<label for="state">State</label>
+							<input type="text" id="state" name="state" />
+						</div>
+
+						<div className="form-group">
+							<label for="zip">Zip Code</label>
+							<input type="text" id="zip" name="zip" />
+						</div>
+
+						<input type="submit" value="Submit Changes" className="btns" />
 					</form>
 
 					<form>
 						<h5>Feed Information</h5>
-						<div className="dropdown">
-							<button className="btn btn-secondary dropdown-toggle div-inline" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Amount
-						</button>
-							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-								<li>Type 1</li>
-								<li>Type 2</li>
-								<li>Type 3</li>
+						<div class="row">
+							<div class="col">
+								<div className="form-group">
+										<label for="org-feed-amount">Amount</label>
+										<select className="form-control" id="org-feed-amount">
+												<option value="" disabled selected>Select feed amount</option>
+												<option>Type 1</option>
+												<option>Type 2</option>
+												<option>Type 3</option>
+										</select>
+								</div>
 							</div>
-							<button type="submit" className="btn btn-xs btn-default add">Add New Amount</button>
+								<div class="col">
+									<button type="submit" className="btns add-org">Add New Amount</button>
+								</div>
 						</div>
 
-
-						<div className="dropdown">
-							<button className="btn btn-secondary dropdown-toggle div-inline" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Unit
-						</button>
-							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-								<li>Type 1</li>
-								<li>Type 2</li>
-								<li>Type 3</li>
-							</div>
-							<button type="submit" className="btn btn-xs btn-default add">Add New Unit</button>
+						<div className="form-group">
+								<label for="org-feed-unit">Unit</label>
+								<select className="form-control" id="org-feed-unit">
+										<option value="" disabled selected>Select feed unit</option>
+										<option>Type 1</option>
+										<option>Type 2</option>
+										<option>Type 3</option>
+								</select>
+								<button type="submit" className="btns add-org">Add New Unit</button>
 						</div>
-
-						<div className="dropdown last">
-							<button className="btn btn-secondary dropdown-toggle div-inline" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Type
-						</button>
-							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-								<li>Type 1</li>
-								<li>Type 2</li>
-								<li>Type 3</li>
-							</div>
-							<button type="submit" className="btn btn-xs btn-default add">Add New Type</button>
+						<div className="form-group">
+								<label for="org-feed-type">Type</label>
+								<select className="form-control" id="org-feed-type">
+										<option value="" disabled selected>Select feed type</option>
+										<option>Type 1</option>
+										<option>Type 2</option>
+										<option>Type 3</option>
+								</select>
+								<button type="submit" className="btns add-org">Add New Unit</button>
 						</div>
 					</form>
 
 					<form>
 						<h5>Medicine Information</h5>
-						<div className="dropdown">
-							<button className="btn btn-secondary dropdown-toggle div-inline" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Timing
-						</button>
-							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-								<li>AM</li>
-								<li>PM</li>
-							</div>
-							<button type="submit" className="btn btn-xs btn-default add">Add New Amount</button>
+						<div className="form-group">
+						    <label for="org-med-amount">Amount</label>
+						    <select className="form-control" id="org-med-amount">
+						        <option value="" disabled selected>Select med amount</option>
+						        <option>Type 1</option>
+						        <option>Type 2</option>
+						        <option>Type 3</option>
+						    </select>
+						    <button type="submit" className="btns add-org">Add New Amount</button>
 						</div>
-
-						<div className="dropdown">
-							<button className="btn btn-secondary dropdown-toggle div-inline" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Amount
-						</button>
-							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-								<li>Type 1</li>
-								<li>Type 2</li>
-								<li>Type 3</li>
-							</div>
-							<button type="submit" className="btn btn-xs btn-default add">Add New Amount</button>
+						<div className="form-group">
+						    <label for="org-med-unit">Unit</label>
+						    <select className="form-control" id="org-med-unit">
+						        <option value="" disabled selected>Select med unit</option>
+						        <option>Type 1</option>
+						        <option>Type 2</option>
+						        <option>Type 3</option>
+						    </select>
+						    <button type="submit" className="btns add-org">Add New Unit</button>
 						</div>
-
-
-						<div className="dropdown">
-							<button className="btn btn-secondary dropdown-toggle div-inline" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Unit
-						</button>
-							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-								<li>Type 1</li>
-								<li>Type 2</li>
-								<li>Type 3</li>
-							</div>
-							<button type="submit" className="btn btn-xs btn-default add">Add New Unit</button>
-						</div>
-
-						<div className="dropdown last">
-							<button className="btn btn-secondary dropdown-toggle div-inline" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Type
-						</button>
-							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-								<li>Type 1</li>
-								<li>Type 2</li>
-								<li>Type 3</li>
-							</div>
-							<button type="submit" className="btn btn-xs btn-default add">Add New Type</button>
+						<div className="form-group">
+						    <label for="org-med-type">Type</label>
+						    <select className="form-control" id="org-med-type">
+						        <option value="" disabled selected>Select med type</option>
+						        <option>Type 1</option>
+						        <option>Type 2</option>
+						        <option>Type 3</option>
+						    </select>
+						    <button type="submit" className="btns add-org">Add New Unit</button>
 						</div>
 					</form>
 				</div>
