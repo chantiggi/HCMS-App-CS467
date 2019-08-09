56fda2b1de40d220e2531094f073a746cdefa95f
Jennifer Aldover <aldover27@gmail.com>
2019-08-09 16:21:20 -0400

    removed forms from manage_users and updated add_user and editdelete_user components; added new components into manage_users: AddUserForm and EditUserForm

--- src/manage_users.js
+++ src/manage_users.js
@@ -3,6 +3,8 @@ import './stylesheets/manage_users.css';
 import './stylesheets/add_user.css';
 import { ManagementTabs } from './managementtabs';
 import { Link } from 'react-router-dom';
+import { AddUserForm } from './add_user';
+import { EditUserForm } from './editdelete_user';
 
 export class ManageUsers extends React.Component {
     render() {
@@ -49,58 +51,12 @@ export class ManageUsers extends React.Component {
                                                 </div>
                                                 <div className="modal-body">
 
-                                                  <form class="no-border">
-                                                      <div className="row">
-                                                          <div className="col-50">
-                                                              <h5>Add New User</h5>
-                                                              <div className="row">
-                                                                  <div className="col">
-                                                                      <label for="fname">First Name</label>
-                                                                      <input type="text" id="fname" name="fname" placeholder="John" />
-                                                                  </div>
-                                                                  <div className="col">
-                                                                      <label for="lname">Last Name</label>
-                                                                      <input type="text" id="lname" name="lname" placeholder="Smith" />
-                                                                  </div>
-                                                              </div>
-
-                                                              <div class="row">
-                                                                  <div class="col">
-                                                                      <label for="email"><i className="fa fa-envelope"></i> Email Address</label>
-                                                                      <input type="text" id="email" name="email" placeholder="john@example.com" />
-                                                                  </div>
-                                                              </div>
-
-                                                              <select className="form-control handlerdropdown">
-                                                                  <option value="level">Select Handler Level</option>
-                                                                  <option value=""></option>
-                                                                  <option value=""></option>
-                                                                  <option value=""></option>
-                                                                  <option value=""></option>
-                                                              </select>
-
-                                                              <div className="form-check">
-                                                                  <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                                                                  <label className="form-check-label" for="exampleCheck1">Is the user an <a href="#" title="User is given access to Manage Preferences" data-toggle="popover" data-trigger="hover" data-content="Some content">Administrator</a>?</label>
-                                                              </div>
-
-
-                                                              <div className="row">
-                                                                  <div className="col-50">
-                                                                      <label for="org"><i className="fa fa-envelope"></i> Organization Name</label>
-                                                                      <input type="text" id="org" name="org" placeholder="Organization Name" />
-                                                                  </div>
-                                                              </div>
-                                                          </div>
-                                                          <input type="submit" value="Add New User" className="btns addbtn" />
-                                                      </div>
-                                                  </form>
+                                                  <EditUserForm />
 
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-
 
                                 </td>
                                 <td>
@@ -141,52 +97,7 @@ export class ManageUsers extends React.Component {
                                 </div>
                                 <div className="modal-body">
 
-                                  <form class="no-border">
-                                      <div className="row">
-                                          <div className="col-50">
-                                              <h5>Add New User</h5>
-                                              <div className="row">
-                                                  <div className="col">
-                                                      <label for="fname">First Name</label>
-                                                      <input type="text" id="fname" name="fname" placeholder="John" />
-                                                  </div>
-                                                  <div className="col">
-                                                      <label for="lname">Last Name</label>
-                                                      <input type="text" id="lname" name="lname" placeholder="Smith" />
-                                                  </div>
-                                              </div>
-
-                                              <div class="row">
-                                                  <div class="col">
-                                                      <label for="email"><i className="fa fa-envelope"></i> Email Address</label>
-                                                      <input type="text" id="email" name="email" placeholder="john@example.com" />
-                                                  </div>
-                                              </div>
-
-                                              <select className="form-control handlerdropdown">
-                                                  <option value="level">Select Handler Level</option>
-                                                  <option value=""></option>
-                                                  <option value=""></option>
-                                                  <option value=""></option>
-                                                  <option value=""></option>
-                                              </select>
-
-                                              <div className="form-check">
-                                                  <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                                                  <label className="form-check-label" for="exampleCheck1">Is the user an <a href="#" title="User is given access to Manage Preferences" data-toggle="popover" data-trigger="hover" data-content="Some content">Administrator</a>?</label>
-                                              </div>
-
-
-                                              <div className="row">
-                                                  <div className="col-50">
-                                                      <label for="org"><i className="fa fa-envelope"></i> Organization Name</label>
-                                                      <input type="text" id="org" name="org" placeholder="Organization Name" />
-                                                  </div>
-                                              </div>
-                                          </div>
-                                          <input type="submit" value="Add New User" className="btns addbtn" />
-                                      </div>
-                                  </form>
+                                  <AddUserForm />
 
                                 </div>
                             </div>
