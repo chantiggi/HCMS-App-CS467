ddabebee162907b075d37f7803346e8bd7b4b133
Chantel Higginbotham <34529538+chantiggi@users.noreply.github.com>
2019-08-11 20:59:11 -0400

    Set up med modals to pull database info on feed page. Still needs work

--- src/PMMedList.js
+++ src/PMMedList.js
@@ -5,25 +5,25 @@ export class PMMedList extends React.Component {
         super(props);
 
         this.state = {
-            meds: []
+            pmmeds: []
         };
     }
 
     componentDidMount() {
         const horseID = this.props.horseID;
-        fetch(`/restapi/am-meds/${horseID}`, {
+        fetch(`/restapi/pm-meds/${horseID}`, {
             method: "GET",
         })
         .then(response => response.json())
-        .then(data => this.setState({meds: data}))
+        .then(data => this.setState({pmmeds: data}))
         .catch(err => console.log("Error reading data: ", err))
     }
     render() {
-        const {meds} = this.state;
+        const {pmmeds} = this.state;
 
         return (
             <ul>
-            {meds.map(currentMed =>
+            {pmmeds.map(currentMed =>
                 <li key={currentMed.horseMedID}>{currentMed.amount} {currentMed.unit} {currentMed.medName} {currentMed.medNotes ? ('(Notes: ' + currentMed.medNotes + ')') : ''}</li>
             )}
             </ul>
