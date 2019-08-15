4cc5c8186cefd6cd8c93627b9007ab0f81cf098b
kosteckiann <kostecka@oregonstate.edu>
2019-08-15 08:33:18 -0700

    Add files via upload

--- server/models/orgModel.js
+++ server/models/orgModel.js
@@ -21,11 +21,50 @@ var Org = function (org) {
                 result(null, err);
             }
             else {
-                console.log("Users returned are: ", res);
+                console.log("Org returned is: ", res);
                 result(null, res);
             }
         });
 };
 
+    Org.addOrg = function (result) {
+    sql.query("SELECT orgName, streetAddress, city, state, zipCode FROM Organization WHERE orgID = 1",
+        function (err, res) {
+            if (err) {
+                console.log("Error with SQL query: ", err);
+                result(null, err);
+            }
+            else {
+                console.log("Org returned is: ", res);
+                result(null, res);
+            }
+        });
+};
+
+
+    Org.getOrgById = function (orgID, result) {
+    sql.query("SELECT orgName, streetAddress, city, state, zipCode FROM Organization WHERE orgID = ?", orgID, function (err, res) {
+            if (err) {
+                console.log("Error with SQL query: ", err);
+                result(null, err);
+            }
+            else {
+                console.log("Org data returned is: ", res);
+                result(null, res);
+            }
+        });
+};
+
+    Org.updateOrgById = function (orgID, org, result) {
+        sql.query('UPDATE Organization SET Organization = ? WHERE orgID = ?', [org.org, orgID], function (err, res) {
+            if (err) {
+                console.log("Error with SQL query: ", err);
+                result(null, err);
+            }
+            else {
+                result(null, res);
+            }
+        });
+    };
 
 module.exports = Org;
