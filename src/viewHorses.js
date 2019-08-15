c451cb4f3015238bd69b818dccb172d3dd0aff7a
Chantel Higginbotham <34529538+chantiggi@users.noreply.github.com>
2019-08-15 00:26:49 -0400

    Updated minor styling on the View All Horses page

--- src/viewHorses.js
+++ src/viewHorses.js
@@ -45,15 +45,17 @@ export class ViewHorsesPage extends React.Component {
                 <div className="all-horses">
                     <div className="row">
                         {horses.map(horse =>
-                            <div className="card horseCard" key={horse.horseID}>
-                                <Link to={`/viewindivhorse/${horse.horseID}`} className="horse-link">
-                                    <img className="card-img-top horse-image" src="https://images.unsplash.com/photo-1553284965-5dd8352ff1bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
-                                    <div className="card-body profile-preview">
-                                    <p className="card-text">Name: {horse.horseName}</p>
-                                    <p className="card-text">Age: {(new Date()).getFullYear() - horse.birthYear} years</p>
-                                    <p className="card-text">Handler Level: {horse.handlerLevelName} </p>
-                                    </div>
-                                </Link>
+                            <div className="col-sm-4">
+                                <div className="card horseCard" key={horse.horseID}>
+                                    <Link to={`/viewindivhorse/${horse.horseID}`} style={{textDecoration: 'none', color: 'black'}} className="horse-link">
+                                        <img className="card-img-top horse-image" src="https://images.unsplash.com/photo-1553284965-5dd8352ff1bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+                                        <div className="card-body profile-preview">
+                                        <p className="card-text">Name: {horse.horseName}</p>
+                                        <p className="card-text">Age: {(new Date()).getFullYear() - horse.birthYear} years</p>
+                                        <p className="card-text">Handler Level: {horse.handlerLevelName} </p>
+                                        </div>
+                                    </Link>
+                                </div>
                             </div>
                         )}
                     </div>
