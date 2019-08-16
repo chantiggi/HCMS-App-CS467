ca79958fbafebaf3deaf89cfba9d95e032e2c467
Jennifer Aldover <aldover27@gmail.com>
2019-08-16 16:50:17 -0400

    updated queries for user model and controller

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -13,15 +13,15 @@ exports.list_all_users = function(req, res) {
 
 // Add a new user
 exports.add_user = function(req, res) {
-    var newUser = new User(req.body);
+    var newUser = req.body;
 
     // handles null error
-    if (!newUser.fname || !newUser.lname || !newUser.email ||
+    if (!newUser.fname || !newUser.lname || !newUser.email || !newUser.username ||
         !newUser.handlerLevelID) {
         res.status(400).send({error: true, message: "Please enter all required fields."});
     }
     else {
-        User.createUser(User, function(err, user) {
+        User.createUser(newUser, function(err, user) {
             if (err) { res.send(err); }
             res.json(user);
         });
@@ -38,7 +38,7 @@ exports.get_a_user = function(req, res) {
 
 // Update an individual users
 exports.update_a_user = function(req, res) {
-    User.updateUserById(req.params.userID, new User(req.body), function(err, user) {
+    User.updateUserById(req.body, function(err, user) {
         if (err) { res.send(err); }
         res.json(user);
     });
