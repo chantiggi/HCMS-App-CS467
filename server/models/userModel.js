6df68e9c15e4ed407b316bcf0b9a01237fdf9074
Jennifer Aldover <aldover27@gmail.com>
2019-08-16 20:16:01 -0400

    corrected createUser to include isActive

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -34,8 +34,8 @@ var User = function (user) {
     //Add User
     User.createUser = function (newUser, result) {
     console.log("newUser", newUser);
-    let sqlQuery = 'INSERT INTO User (fname, lname, email, username, handlerLevelID, isAdmin, orgID) ' +
-    'VALUES (?, ?, ?, ?, ?, ?, ?)';
+    let sqlQuery = 'INSERT INTO User (fname, lname, email, username, handlerLevelID, isAdmin, isActive, orgID) ' +
+    'VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
     sql.query(sqlQuery, [newUser.fname, newUser.lname, newUser.email, newUser.username, newUser.handlerLevelID, newUser.isAdmin, newUser.orgID],
     function (err, res) {
         if (err) {
